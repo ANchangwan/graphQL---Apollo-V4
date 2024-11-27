3fdecf2ec8e7c246956b586718a919cf54a056ac
waney <ssdii66@gmail.com>
2024-11-27 20:43:55 +0900

    graph ql

--- server.js
+++ server.js
@@ -6,35 +6,46 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 let tweets = [
     {
         id: "1",
-        text: "This is the first query",
-        author: {
-            id: "1",
-            name: "changwan"
-        },
+        text: "first one!",
+        userId: "2",
     },
     {
         id: "2",
-        text: "This is the second query",
-        author: {
-            id: "2",
-            name: "changwan2"
-        }
+        text: "second one",
+        userId: "1",
+    },
+];
+let users = [
+    {
+        id: "1",
+        firstName: "changwan",
+        lastName: "An",
+        fullName:"An changwan"
+    },
+    {
+        id: "2",
+        firstName: "minju",
+        lastName: "Kim",
+        fullName:"Kim minJu"
     }
 ]
 
 const typeDefs = `
     type User {
-        id: ID,
-        name: String,
+        id: ID!,
+        firstName: String,
+        lastName: String,
+        fullName: String,
     }
     type Tweet {
         id: ID,
         text: String,
-        author: User
+        author: User,
     }
     type Query {
        allTweets: [Tweet],
        tweet(id: ID!): Tweet,
+       allUsers: [User],
        ping: String
     }
     type Mutation {
@@ -47,12 +58,15 @@ const resolvers = {
     Query: {
         allTweets: () => tweets,
         tweet(root, {id}){
-            console.log(id);
             return tweets.find(tweet => tweet.id === id);
         },
         ping(){
             return "pong";
-        }
+        },
+        allUsers(){
+            return users;
+        },
+
     },
     Mutation:{
         postTweet(root, {userId, text}){
@@ -63,13 +77,26 @@ const resolvers = {
             tweets.push(newTweet);
             return newTweet;
         },
-        deleteTweet(_, { Id }) {
-            const tweet = tweets.find((tweet) => tweet.id === Id);
+        // 변수명 통일 typedef === resolvers
+        deleteTweet(_, { userId }) {
+            const tweet = tweets.find((tweet) => tweet.id === userId);
             if (!tweet) return false;
-            tweets = tweets.filter((tweet) => tweet.id !== Id);
+            tweets = tweets.filter((tweet) => tweet.id !== userId);
             return true;
         },
+    },
+    Tweet: {
+        author({userId} ) {
+            console.log(userId)
+            return users.find((user) => user.id === userId);
+        },
+    },
+    User:{
+        fullName(){
+            return "hello";
+        }
     }
+
 };
 
 // type 변수명과 resolve 변수명 통일
